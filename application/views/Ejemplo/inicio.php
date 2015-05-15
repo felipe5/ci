9e60c8dfe0a857b16331baeb9f67df651b20afbe
Felipe H <felipe.herrera@outlook.com>
2015-05-15 15:52:19 -0300

    comentado

--- application/views/Ejemplo/inicio.php
+++ application/views/Ejemplo/inicio.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<body>
-		<a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_simple">Consulta Simple</a><br>
+		<!-- <a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_simple">Consulta Simple</a><br> -->
 		<a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_media">Consulta Media</a><br>
 		<a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_compleja">Consulta Compleja</a>
 	</body>
