ce90dceddca0d53ed23245ad984bbc1695a6616f
rosavelasquezrojas <rvelasquez.rojas@gmail.com>
2015-05-15 15:52:06 -0300

    prueba

--- application/views/Ejemplo/inicio.php
+++ application/views/Ejemplo/inicio.php
@@ -2,7 +2,7 @@
 <html>
 	<body>
 		<a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_simple">Consulta Simple</a><br>
-		<a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_media">Consulta Media</a><br>
+		<a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_media">Consulta Mediana</a><br>
 		<a href="<?php echo base_url() ?>/index.php/ejemploController/consulta_compleja">Consulta Compleja</a>
 	</body>
 </html>
